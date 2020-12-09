69d5c587f20212ccfc80d058130fd731401fb526
Oskar Anczyk <myamy45.5@gmail.com>
2020-12-09 08:50:53 +0100

    6.2 Added class Calculator and tested it's methods - add() and substract()

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("Test OK");
+        }else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int a = 3;
+        int b = 2;
+        if(a + b == calculator.add(a,b)){
+            System.out.println("addition is correct");
+        } else {
+            System.out.println("addition is not correct");
+        }
+
+        if(a - b == calculator.substract(a,b)){
+            System.out.println("substraction is correct");
+        } else {
+            System.out.println("substraction is not correct");
+        }
     }
 }
