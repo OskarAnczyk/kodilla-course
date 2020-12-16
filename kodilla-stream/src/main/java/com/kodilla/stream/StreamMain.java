14f41ba216d1c2a46f0b48cb0918befe39723d6c
Oskar Anczyk <myamy45.5@gmail.com>
2020-12-16 09:48:57 +0100

    7.2 done

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
@@ -16,5 +17,8 @@ public class StreamMain {
         poemBeautifier.beautify(poem,(s) -> s.toUpperCase());
         poemBeautifier.beautify(poem,(s) -> s.toLowerCase());
         poemBeautifier.beautify(poem,(s) -> s + ".\n" + s + ".");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
