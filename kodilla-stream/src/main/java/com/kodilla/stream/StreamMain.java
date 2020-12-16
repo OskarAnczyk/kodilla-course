2140395d8b53018dea70ddadf95fa25151db1398
Oskar Anczyk <myamy45.5@gmail.com>
2020-12-16 09:28:54 +0100

    7.1 done

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        String poem = "Szlachetne zdrowie";
+        poemBeautifier.beautify(poem,(s) -> "ABC" + s + "ABC");
+        poemBeautifier.beautify(poem,(s) -> s.toUpperCase());
+        poemBeautifier.beautify(poem,(s) -> s.toLowerCase());
+        poemBeautifier.beautify(poem,(s) -> s + ".\n" + s + ".");
+    }
+}
